53f574a3e300163697b844356c960588dd9209c9
Stephanie <s.a.james5@gmail.com>
2018-06-22 21:03:30 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -42,17 +42,7 @@ def play(board)
   
   until turn_count == 9 
   
-  puts "Please enter 1-9"
-  
-  input = gets.strip
-  
-  index = input_to_index(input)
-  
-  valid_move?(board, index)
-
-  move(board, index)
-
-  display_board(board)
+  turn(board)
   
   turn_count +=1
 
